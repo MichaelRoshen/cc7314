b426afdf4a3f7e532558e336c65be62bcedf643f
超子 <cc90146@alibaba-inc.com>
2016-05-20 17:22:14 +0800

    添加管理员

--- app/controllers/plainpage_controller.rb
+++
@@ -1,4 +0,0 @@
-class PlainpageController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'plainpage/index'
+  resources :users
+  get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
